821c3695a95754a44df2ebc2d126bdfb81b44b75
taejinseok <stj930@gmail.com>
2020-06-28 14:40:33 +0900

    init: init racing car project

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    // TODO: 2020/06/28 추가기능 구현 
+}
