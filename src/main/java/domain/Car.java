c959fda136de335ad382848b2a34d983df2443b0
jslee <dlwhdtjd098@naver.com>
2020-07-05 21:11:30 +0900

    Car 기능 추가
    1. Play 메소드
    2. MoveOrStop 메소드 추가
    3. checkLength 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -5,8 +5,33 @@ public class Car {
     private int position = 0;
 
     public Car(String name) {
+        if(!checkLength(name)) {
+            throw new IllegalArgumentException("이름은 5자 이내로 설정해주세요.");
+        }
         this.name = name;
     }
 
-    // TODO: 2020/06/28 추가기능 구현 
+    // TODO: 2020/06/28 추가기능 구현
+
+    public int play (){
+        if(moveOrStop()) {
+            position++;
+        }
+        System.out.println(name + " : " + position);
+        return position;
+    }
+    private Boolean moveOrStop() {
+        int random = (int)(Math.random() * 10);
+        if (random <= 3) {
+            return false;
+        }
+        return true;
+    }
+    private Boolean checkLength (String name) {
+        if (name.length() > 5) {
+            return false;
+        }
+        return true;
+    }
+
 }
