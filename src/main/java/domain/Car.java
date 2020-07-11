d9fb2c901ac05c4633c59c7525d9dc9617955446
jslee <dlwhdtjd098@naver.com>
2020-07-11 12:15:30 +0900

    car - racing 기능 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -5,7 +5,7 @@ public class Car {
     private int position = 0;
 
     public Car(String name) {
-        if(!checkLength(name)) {
+        if (!checkLength(name)) {
             throw new IllegalArgumentException("이름은 5자 이내로 설정해주세요.");
         }
         this.name = name;
@@ -13,25 +13,33 @@ public class Car {
 
     // TODO: 2020/06/28 추가기능 구현
 
-    public int play (){
-        if(moveOrStop()) {
+    public int play() {
+        if (moveOrStop()) {
             position++;
         }
-        System.out.println(name + " : " + position);
         return position;
     }
+
     private Boolean moveOrStop() {
-        int random = (int)(Math.random() * 10);
+        int random = (int) (Math.random() * 10);
         if (random <= 3) {
             return false;
         }
         return true;
     }
-    private Boolean checkLength (String name) {
+
+    private Boolean checkLength(String name) {
         if (name.length() > 5) {
             return false;
         }
         return true;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,30 +1,57 @@
 package domain;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] names = sc.next().replace(" ", "").split(",");
-        Map<Integer, Car> hm = new HashMap<>();
-        for (int i = 0; i < names.length; i++) {
-            new Car(names[i]);
-        }
-        for (Map.Entry<Integer, Car> entry : hm.entrySet()) {
-            entry.getValue().play();
+        while (true) {
+            HashMap<String, Car> player = new HashMap<>();
+            System.out.println("이름을 입력하세요.");
+            String[] names = sc.next().replace(" ", "").split(",");
+            System.out.println("횟수를 입력하세요.");
+            String coin_ = sc.next();
+            int coin = Integer.parseInt(coin_);
+            for (int i = 0; i < names.length; i++) {
+                player.put(names[i], new Car(names[i]));
+            }
+            goal(coin, player);
         }
     }
 
-    private Integer NUM = 0;
-
-    private String Goal(int coin, HashMap<Integer, Car> hm) {
-        while (NUM <= coin) {
-            for (Map.Entry<Integer, Car> entry : hm.entrySet()) {
-                entry.getValue().play();
+    private static void goal(int coin, HashMap<String, Car> player) {
+        int num = 0;
+        String mark = ">";
+        while (num < coin) {
+            for (Map.Entry<String, Car> entry : player.entrySet()) {
+                int position = entry.getValue().play();
+                String markRepeated = (String.join("", Collections.nCopies(position, mark)));
+                System.out.println(entry.getKey() + ":" + markRepeated);
             }
-            NUM++;
+            num++;
         }
-        return "";
+        System.out.println("우승자는 " + winner(player));
+    }
+
+    private static String winner(HashMap<String, Car> player) {
+        int maxNum = 0;
+        String winner = "";
+        for (Map.Entry<String, Car> entry : player.entrySet()) {
+            int position = entry.getValue().getPosition();
+            if (maxNum < position) {
+                maxNum = position;
+            }
+        }
+        for (Map.Entry<String, Car> entry : player.entrySet()) {
+            if (entry.getValue().getPosition() == maxNum) {
+                winner += entry.getKey() + ",";
+            }
+        }
+        winner = winner.substring(0, winner.length() - 1);
+        return winner;
     }
 }
 
